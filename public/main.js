7f348b360fe1ec6e9a08b4f7cc075ecf5fa36994
Vu Hung <phamvuhung2710@gmail.com>
2022-08-12 17:57:26 +0700

    responsive header

---
+++ public/main.js
@@ -0,0 +1,14 @@
+const topMenu = document.getElementById('vhd-top-menu');
+const toggleTopMenuIcon = document.getElementById('vhd-toggle-top-menu-icon');
+
+document.addEventListener('click', (e) => {
+  if (toggleTopMenuIcon.contains(e.target)) {
+    topMenu.classList.toggle('vhd-top-menu-expanded');
+    topMenu.classList.toggle('hidden');
+  } else {
+    if (topMenu.classList.contains('vhd-top-menu-expanded')) {
+      topMenu.classList.remove('vhd-top-menu-expanded');
+      topMenu.classList.add('hidden');
+    }
+  }
+});
