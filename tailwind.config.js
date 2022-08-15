cc549221c7433dc8b3bc2e4c85c320ae5ce48fcd
Vu Hung <phamvuhung2710@gmail.com>
2022-08-15 16:51:58 +0700

    add featured mugs - product card

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,31 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        Karla: ['Karla', 'sans-serif']
+        Karla: ['Karla', 'sans-serif'],
       },
       colors: {
         coffee: {
           50: '#E8D6D0',
           200: '#C89F94',
           400: '#A25F4B',
-          600: '#744838'
-        }
+          600: '#744838',
+        },
       },
       keyframes: {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' }
-        }
+          '100%': { transform: 'translateY(0)' },
+        },
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
       },
       animation: {
-        slideDown: 'slideDown .4s ease-in-out'
-      }
-    }
+        slideDown: 'slideDown .5s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out',
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
